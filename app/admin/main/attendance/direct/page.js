142c176c8f346e02f4ffe3c732c5ca4088d50285
zbytes2227 <zbytes1@outlook.com>
2024-02-07 09:55:12 +0530

    new updates2

--- app/admin/main/attendance/direct/page.js
+++ app/admin/main/attendance/direct/page.js
@@ -8,6 +8,7 @@ export default function Home() {
   const results = [];
   const [FinalARR, setFinalARR] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [DeviceIP, setDeviceIP] = useState("192.168.1")
 
   const searchQueryLowercase = searchQuery.toLowerCase();
   const filteredCards = FinalARR.filter(
@@ -89,43 +90,32 @@ export default function Home() {
     }
   };
 
+const [Connecting, setConnecting] = useState(false);
+
+  const connectToMachine = () => {
+    setConnecting(true);
+    getFileNames();
+  }
 
   const getFileNames = async () => {
     try {
-      const response = await fetch("http://192.168.1.9", {
+      const response = await fetch(`http://${DeviceIP}`, {
         method: "GET",
-        // mode: 'no-cors',
         headers: {
           "Content-Type": "application/json",
-
         },
-        // heade/ccess-Control-Allow-Credentials', 'true');
-        // // body: JSON.stringify({ ids }),
       });
 
       if (response) {
-        // const responseData = await response.json();
-        console.log(response);
-        // console.log("responseData");
-        // console.log(responseData);
+        const data = await response.json();
+        const fileNames = data.filter((item) => item.file).map((item) => item.file);
+        console.log(fileNames);
         
-        // response.forEach(item => {
-        //     // Check if the item has a "folder" property
-        //     if (item.folder) {
-        //         console.log(`Folder: ${item.folder}`);
-        //     } else if (item.file) {
-        //         // Check if the item has a "file" property
-        //         console.log(`File: ${item.file}`);
-        //     }
-        // });
-         
    
       } else {
-        // Handle other response statuses (e.g., 404 Not Found, 500 Internal Server Error)
         console.error("Failed to fetch attendance. Status:", response.status);
       }
     } catch (error) {
-      // Handle network or other errors
       console.error("Error fetching attendance:", error);
     }
   };
@@ -186,7 +176,68 @@ export default function Home() {
 
   return (
     <div className="p-4 sm:ml-64 mt-14">
-      <form>
+      
+
+        <label
+          for="default-search"
+          class="mb-2 text-sm font-medium text-gray-900 sr-only"
+        >
+          Connexct
+        </label>
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              class="w-4 h-4 text-gray-500 "
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            value={DeviceIP}
+            onChange={(e) => {
+              setDeviceIP(e.target.value);
+            }}
+            id="default-search"
+            class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 outline-none"
+            placeholder="192.168.1.X"
+            required
+          />
+          <button
+            type="submit"
+            onClick={connectToMachine}
+            class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+          >
+            Connect
+          </button>
+        </div>
+
+
+
+
+
+   
+
+      {/* {idList.length > 0 && (
+        <div>
+          <h2 className="text-lg font-semibold mt-4 mb-2">Extracted IDs</h2>
+          <pre>{JSON.stringify(idList, null, 2)}</pre>
+        </div>
+
+      )} */}
+
+      {filteredCards.length > 0 ?(
+        <form>
         <label
           for="default-search"
           class="mb-2 text-sm font-medium text-gray-900 sr-only"
@@ -230,32 +281,8 @@ export default function Home() {
           </button>
         </div>
       </form>
+      ):""}
 
-      <div className="flex my-3 items-center justify-end">
-        {/* {file && (
-          <div>
-            <h2 className="text-lg font-semibold mb-2">Uploaded CSV</h2>
-            <p>{file.name}</p>
-          </div>
-        )} */}
-        <input
-          type="file"
-          accept=".csv"
-          onChange={handleFileChange}
-          class=" text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-md px-5 py-2 text-center"
-        />
-        <button onClick={getFileNames} class="mx-4 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-md px-5 py-2 text-center">
-          Refresh
-        </button>
-      </div>
-
-      {/* {idList.length > 0 && (
-        <div>
-          <h2 className="text-lg font-semibold mt-4 mb-2">Extracted IDs</h2>
-          <pre>{JSON.stringify(idList, null, 2)}</pre>
-        </div>
-
-      )} */}
        <div className="relative overflow-x-auto shadow-md sm:rounded-lg h-screen">
 
     
