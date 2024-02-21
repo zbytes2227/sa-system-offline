2ab1d7c2f258b0dc164bc52bd253a76ccd496ac2
zbytes2227 <zbytes0@gmail.com>
2024-02-21 12:44:39 +0530

    finalss

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,8 @@ const connectDb = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
-  await mongoose.connect("mongodb+srv://zbytes1:OMrHtXsW9EdP5xSf@cluster0.45jhcsq.mongodb.net/kbstest?retryWrites=true&w=majority");
+  await mongoose.connect(process.env.MONGO_URI);
+
   return handler(req, res);
 };
 export default connectDb;
